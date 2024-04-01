8ce752b645f45f3041ddb8de19c8dba372d3813e
Quisie Caporte <quisie.caporte@g.msuiit.edu.ph>
2024-04-02 00:19:57 +0800

    weekly and total charts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,76 +1,78 @@
-import React, { useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import Navbar2 from '../components/Navbar2'
-import Cards from '../components/Cards'
-import Logs from '../components/Logs'
-import Chart from '../components/Chart'
+import React, { useState, useEffect } from "react";
+import Sidebar from "../components/Sidebar";
+import Navbar2 from "../components/Navbar2";
+import Cards from "../components/Cards";
+import Logs from "../components/Logs";
+import Chart from "../components/Chart";
 
+// added
+import Total from "./Total";
 
 const Dashboard = () => {
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
 
-    const toggleSidebar = () => {
-        setSidebarCollapsed(!sidebarCollapsed);
-    };
+  const toggleSidebar = () => {
+    setSidebarCollapsed(!sidebarCollapsed);
+  };
 
   return (
     <div>
-
       <div className="wrapper ">
         <Sidebar sidebarCollapsed={sidebarCollapsed} />
         <div className="main">
           <Navbar2 toggleSidebar={toggleSidebar} />
           <main className="content px-3 py-2 bg-secondary bg-opacity-10">
             <div className="container-fluid">
-
               <div className="mb-3">
-                <h1 className='fw-bold'>Dashboard</h1>
+                <h1 className="fw-bold">Dashboard</h1>
                 <nav aria-label="breadcrumb">
                   <ol className="breadcrumb">
-                    <li className="breadcrumb-item"><a href="#">Home</a></li>
-                    <li className="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    <li className="breadcrumb-item">
+                      <a href="#">Home</a>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                      Dashboard
+                    </li>
                   </ol>
                 </nav>
               </div>
 
-              <Cards/>
+              <Cards />
 
               <div className="row">
                 <div className="col-md-8">
                   <div className="row mb-4">
-                      <div className="col-md-12 d-flex align-items-center">
-                        <div className="col-8 col-md-8">
+                    <div className="col-md-12 d-flex align-items-center">
+                      <div className="col-8 col-md-8">
                         <div className="heading-text">
-                            <h3 className="lh-1 fw-bold">Realtime Analytics</h3>
-                            <p>Write a short description here.</p>
-                          </div>
+                          <h3 className="lh-1 fw-bold">Realtime Analytics</h3>
+                          <p>Write a short description here.</p>
                         </div>
+                      </div>
 
-                        <div className="col-4 col-md-4 ">
-                          <div className="d-flex justify-content-end ">
-                            <a href="#">view all</a>
-                          </div>
+                      <div className="col-4 col-md-4 ">
+                        <div className="d-flex justify-content-end ">
+                          <a href="#">view all</a>
                         </div>
                       </div>
                     </div>
-                  <Chart/>
+                  </div>
+                  {/* added */}
+                  <Total />
 
-                  <Chart/>
+                  <Chart />
                 </div>
 
                 <div className="col-md-4">
-                 <Logs/>
+                  <Logs />
                 </div>
               </div>
-
             </div>
           </main>
-
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Dashboard
+export default Dashboard;
